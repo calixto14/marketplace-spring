86a52bdac27998514d9ea0ac356a4867ad011fed
gustavo calixto <calixto14@live.com>
2025-01-16 14:14:53 -0300

    first commit

---
+++ src/main/java/com/marktplace/demo/MarktplaceApplication.java
@@ -0,0 +1,13 @@
+package com.marktplace.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarktplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarktplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marktplace/demo/MarktplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marktplace.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarktplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
